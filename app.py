56c5f22a309d6b521ae3201510fe90137e822ef0
SANJAY <90376981+itz-sanjay@users.noreply.github.com>
2022-11-22 19:28:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,5 +125,9 @@ def forget():
             return redirect(url_for("changepass"))
 
 
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0',port=5000,debug=True)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
